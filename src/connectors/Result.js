b566854a09443ca51405ec275de465e51220fcc8
Andreas Åhlund <andreas.ahlund@schibsted.se>
2017-08-25 09:11:55 +0200

    Get total score from state

--- src/connectors/Result.js
+++ src/connectors/Result.js
@@ -1,19 +1,6 @@
 import { connect } from 'react-redux'
 import ResultLayout from '../layouts/Result'
 
-const getTotalScore = (holes) => {
-    if (holes && Object.keys(holes).length > 0) {
-        //create-react-app does not seem to support es2017 for the moment
-        //return Object.values(holes).reduce((a, b) => a + b)
-        let score = 0
-        for (let key of Object.keys(holes)) {
-            score += holes[key]
-        }
-        return score
-    } else {
-        return 0
-    }
-}
 
 const getLastHole = (holes) => {
     if (holes) {
@@ -25,7 +12,7 @@ const getLastHole = (holes) => {
 }
 
 const mapStateToProps = (state) => ({
-    score: getTotalScore(state.holes),
+    score: state.results.totalScore,
     lastHole: getLastHole(state.holes)
 })
 
