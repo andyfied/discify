fdac2e4c84d371e5b5757bac81d1798c97073e4e
Andreas Åhlund <andreas.ahlund@schibsted.se>
2017-10-27 12:21:21 +0200

    Add some styles to login wall

--- src/layouts/LoginWall/LoginWall.js
+++ src/layouts/LoginWall/LoginWall.js
@@ -3,20 +3,25 @@ import React from 'react'
 import FacebookLogin from 'react-facebook-login'
 import config from '../../config/facebook'
 
+import './LoginWall.css'
+
 
 const LoginWall = ({loginCallback}) =>
 {
   console.log(loginCallback)
   return (
-    <div>
-      <h1>Please login</h1>
-      <FacebookLogin
-        appId={config.appId}
-        autoLoad={config.autoLoad}
-        fields={config.fields}
-        //onClick={componentClicked}
-        callback={loginCallback}
+    <div className="LoginWall wrapper">
+      <div className="main">
+        <h1>Discify</h1>
+        <h2>Your number one score tracker</h2>
+        <FacebookLogin
+          appId={config.appId}
+          autoLoad={config.autoLoad}
+          fields={config.fields}
+          //onClick={componentClicked}
+          callback={loginCallback}
       />
+      </div>
     </div>
   )
 } 
